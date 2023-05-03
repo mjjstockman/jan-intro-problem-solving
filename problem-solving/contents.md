5dd52f796b04b7242d5fc9f9f7e83866748432ac
Katherine <hurstkatherine@hotmail.co.uk>
2023-05-03 10:52:30 +0100

    added problem solving files

---
+++ problem-solving/contents.md
@@ -0,0 +1,21 @@
+# Problem Solving
+
+This is a chance to test your problem solving skills. There are some scenarios here designed to emulate real world problems that will require you to translate these situations into code and implement a solution.
+
+Please work on the sections in numerical order.
+
+## Section 1
+
+Please work on `exercises.js` first and `challenges.js` second.
+
+## Section 2
+
+Each of the tasks in this section are prefaced with a number - you should complete them in numerical order.
+
+These challenges are a little tricker than the ones in section 1. Make sure you are applying the principles of problem solving, and you are using pseudo-code to break down your solution into individual logical steps along the way!
+
+## Section 3 - Advanced
+
+Well done if you got this far! Just like in Section 2, please work through the challenges in numerical order.
+
+_Note: There is a lot to get through here we do not expect you to get through them all! As with all of the content we provide we purposely give you more than is doable in the time so you have plenty of extra content to come back to should you wish._
---
+++ problem-solving/section-1/exercises.js
@@ -0,0 +1,126 @@
+const { check, runTest, skipTest } = require("../../test-api/index.js");
+
+/*
+Instructions
+
+For these challenges complete the body of each function in turn.
+Run this file with Node to run the tests and check your function works correctly.
+Take a look at the tests to see what each function should be returning given various arguments.
+When you're ready to move on to the next function replace skipTest with runTest.
+*/
+
+// QUESTION 1
+
+function connectStrings() {
+  // This function should take 2 strings and join them together with a space in between
+  // return this newly created string
+}
+
+runTest("connectStrings() can join 2 strings together", function () {
+  check(connectStrings("hello", "world")).isEqualTo("hello world");
+  check(connectStrings("paul", "rogerson")).isEqualTo("paul rogerson");
+  check(connectStrings("blue", "sky")).isEqualTo("blue sky");
+});
+
+// QUESTION 2
+
+function checkWordEndsWithIng() {
+  // take a string as an argument and return a boolean based on whether the word given ends with 'ing'
+}
+
+skipTest(
+  "checkWordEndsWithIng() can return true/false for a single word",
+  function () {
+    check(checkWordEndsWithIng("doing")).isEqualTo(true);
+    check(checkWordEndsWithIng("eating")).isEqualTo(true);
+    check(checkWordEndsWithIng("bang")).isEqualTo(false);
+    check(checkWordEndsWithIng("singer")).isEqualTo(false);
+  }
+);
+
+// QUESTION 3
+
+function addMissingPunctuation() {
+  // take a string as an argument
+  // each string may end with a full-stop, exclamation mark, or question mark
+  // if the string doesn't end with punctuation, return the string with a full-stop added at the end. Otherwise, return the string unchanged
+}
+
+skipTest(
+  "addMissingPunctuation() returns the correct string length",
+  function () {
+    check(addMissingPunctuation("Hello there!")).isEqualTo("Hello there!");
+    check(addMissingPunctuation("How's it going?")).isEqualTo(
+      "How's it going?"
+    );
+    check(addMissingPunctuation("Yeah I'm good")).isEqualTo("Yeah I'm good.");
+    check(addMissingPunctuation("Nice.")).isEqualTo("Nice.");
+  }
+);
+
+// QUESTION 4
+
+function getRemainder() {
+  // This function should take two arguments a and b, and return the remainder of the division of a / b
+}
+
+skipTest("getRemainder() returns the correct remainder", function () {
+  check(getRemainder(10, 2)).isEqualTo(0);
+  check(getRemainder(119, 10)).isEqualTo(9);
+  check(getRemainder(50, 6)).isEqualTo(2);
+});
+
+// QUESTION 5
+
+function accessObject(obj, key) {
+  // This function should take an object and a key as its arguments and return the value found at the provided key in the input object
+  // If the key doesn't exist on the object, this function should return a string of "property not found"
+}
+
+skipTest("accessObject() can access a property value using a key", function () {
+  check(accessObject({ name: "jonny", age: 32 }, "name")).isEqualTo("jonny");
+  check(accessObject({ name: "jonny", age: 32 }, "age")).isEqualTo(32);
+  check(accessObject({ name: "jonny", age: 32 }, "email")).isEqualTo(
+    "property not found"
+  );
+});
+
+// QUESTION 6
+
+function makeAllWordsBold(arr) {
+  // In markdown files (e.g. 'README.md') we can denote words as bold by putting two asterisks on either side of them, such as: **hello**
+  // This function should take an array of strings as an argument and return an array consisting of the same strings but in bold - ie with two asterisks either side of them
+}
+
+skipTest(
+  "makeAllWordsBold() can convert all strings to be surrounded by double asterisks",
+  function () {
+    check(makeAllWordsBold(["hello", "there", "world"])).isEqualTo([
+      "**hello**",
+      "**there**",
+      "**world**"
+    ]);
+    check(makeAllWordsBold(["I", "love", "coding"])).isEqualTo([
+      "**I**",
+      "**love**",
+      "**coding**"
+    ]);
+  }
+);
+
+// QUESTION 7
+
+function getPositiveNumbers(arr) {
+  // This function should take an array of numbers as an argument and return an array containing all positive numbers from the input (retaining the order)
+}
+
+skipTest(
+  "getPositiveNumbers() can get all the positive numbers from an array of numbers",
+  function () {
+    check(getPositiveNumbers([1, -1, 2, -2, 3, -3])).isEqualTo([1, 2, 3]);
+    check(getPositiveNumbers([-80, 9, 100, 13, 20, -7])).isEqualTo([
+      9, 100, 13, 20
+    ]);
+    check(getPositiveNumbers([-1, -50, -999])).isEqualTo([]);
+  }
+);

fa42e85dc0055855ba3e3defbd6b97ad5df575db
mjjstockman <mjjstockman@gmail.com>
2025-01-23 14:38:49 +0000

    complete section-3

--- section-3/10-fold-array.js
+++ section-3/10-fold-array.js
@@ -1,4 +1,4 @@
-const { check, runTest, skipTest } = require("../test-api/index.js");
+const { check, runTest, skipTest } = require('../test-api/index.js');
 
 /*
 This function folds an array in the middle n number of times.
@@ -14,27 +14,42 @@ Fold 1-times:
 [1,2,3,4,5] -> [6,6,3]
 Here we fold the 1st with the last and the second with the 4th. As it is odd in length, the middle index is not folded
 */
-function foldArray() {}
+function foldArray(array, runs = 1) {
+  if (runs === 0) return array;
 
-console.log("foldArray");
+  const foldedArray = [];
+  const mid = Math.floor(array.length / 2);
 
-runTest("folds a even length array", function () {
+  for (let i = 0; i < mid; i++) {
+    foldedArray.push(array[i] + array[array.length - 1 - i]);
+  }
+
+  if (array.length % 2 !== 0) {
+    foldedArray.push(array[mid]);
+  }
+
+  return foldArray(foldedArray, runs - 1);
+}
+
+console.log('foldArray');
+
+runTest('folds a even length array', function () {
   check(foldArray([1, 2], 1)).isEqualTo([3]);
   check(foldArray([1, 2, 3, 10, 34, 100], 1)).isEqualTo([101, 36, 13]);
 });
 
-skipTest("folds an odd length array", function () {
+runTest('folds an odd length array', function () {
   check(foldArray([1, 2, 3], 1)).isEqualTo([4, 2]);
 });
 
-skipTest("folds an even length array multiple times", function () {
+runTest('folds an even length array multiple times', function () {
   check(foldArray([1, 2, 3, 10, 34, 100], 2)).isEqualTo([114, 36]);
 });
 
-skipTest("folds an array to a single value", function () {
+runTest('folds an array to a single value', function () {
   check(foldArray([1, 2, 3, 10, 34, 100], 3)).isEqualTo([150]);
 });
 
-skipTest("repeated folds remain the same", function () {
+runTest('repeated folds remain the same', function () {
   check(foldArray([1, 2, 3, 10, 34, 100], 4)).isEqualTo([150]);
 });
--- section-3/9-unique-and-ordered.js
+++ section-3/9-unique-and-ordered.js
@@ -1,43 +1,50 @@
-const { check, runTest, skipTest } = require("../test-api/index.js");
+const { check, runTest, skipTest } = require('../test-api/index.js');
 
 /*
 Implement a function which takes as an argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
 The function should be able to to work with both strings and arrays, and should return an array.
 */
-function uniqueAndOrdered() {}
+function uniqueAndOrdered(nums) {
+  const uniqueAndOrdered = [];
 
-console.log("uniqueAndOrdered");
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      uniqueAndOrdered.push(nums[i]);
+    }
+  }
+  return uniqueAndOrdered;
+}
 
-runTest("returns unique ordered numbers from an array", function () {
+runTest('returns unique ordered numbers from an array', function () {
   check(uniqueAndOrdered([1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 1, 1])).isEqualTo([
     1, 2, 3, 1
   ]);
 });
 
-skipTest("returns unique ordered letters from a string", function () {
-  check(uniqueAndOrdered("nnoorrtthhccooddeerrss")).isEqualTo([
-    "n",
-    "o",
-    "r",
-    "t",
-    "h",
-    "c",
-    "o",
-    "d",
-    "e",
-    "r",
-    "s"
+runTest('returns unique ordered letters from a string', function () {
+  check(uniqueAndOrdered('nnoorrtthhccooddeerrss')).isEqualTo([
+    'n',
+    'o',
+    'r',
+    't',
+    'h',
+    'c',
+    'o',
+    'd',
+    'e',
+    'r',
+    's'
   ]);
 });
 
-skipTest("is case sensitive for strings", function () {
-  check(uniqueAndOrdered("AaAAABBBCCCc")).isEqualTo([
-    "A",
-    "a",
-    "A",
-    "B",
-    "C",
-    "c"
+runTest('is case sensitive for strings', function () {
+  check(uniqueAndOrdered('AaAAABBBCCCc')).isEqualTo([
+    'A',
+    'a',
+    'A',
+    'B',
+    'C',
+    'c'
   ]);
 });
